9834440066bd202a8b20224678f8c90827c74037
elliot <elliotgcraig@icloud.com>
2019-07-03 21:32:10 -0600

    contact

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,7 +3,47 @@ import React from 'react';
 function Home() {
     return (
         <div>
-            <p>Sunny's App</p>
+            <div className='home-top'>
+                <div>
+                    <div>
+                        Sunny Stewert
+                    </div>
+                </div>
+                <div>
+                    <nav>
+                        <div>
+                            Pottery
+                        </div>
+                        <div>
+                            Lifestyle
+                        </div>
+                        <div>
+                            Bio
+                        </div>
+                        <div>
+                            Contact
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div className='home-mid'>
+                <div>
+                    <div className='picture_block-a'>
+
+                    </div>
+                    <div className='picture_block-b'>
+
+                    </div>
+                    <div className='picture_block-c'>
+
+                    </div>
+                </div>
+            </div>
+            <div className='home-bottom'>
+                <div className='contact'>
+                    <p>555 555-5555</p>
+                </div>
+            </div>
         </div>
     );
 };
